f81d5390b6d231bdd341969ae7bb3bec0c91e316
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-01 01:09:23 -0500

    Adding a body field to idea submission form.

--- app/controllers/ideas/index.js
+++ app/controllers/ideas/index.js
@@ -2,19 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	ideaTitle: "",
+	ideaBody: "",
 	actions: {
 		addIdea: function() {
 			this.store.find('idea').then(function(ideas){
 				this.store.push('idea', {
 					id: ideas.get('length') + 1,
 					title: this.get('ideaTitle'),
-					body: "Content for " + this.get('ideaTitle'),
+					body: this.get('ideaBody'),
 				});
 				this.set('ideaTitle', '');
+				this.set('ideaBody', '');
 			}.bind(this));
 		},
 		delIdea: function(idea) {
 			idea.destroyRecord();
-		}
+		},
 	}
 });
