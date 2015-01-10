ec5c978b47e19e69169320833c7cbb5eba8b3b26
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-09 23:35:58 -0500

    Adding ability to post comments.

---
+++ app/controllers/ideas/idea.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	commentAuthor: '',
+	commentBody: '',
+	actions: {
+		postComment: function(model) {
+			this.store.push('comment', {
+				id: String(Math.random()).slice(2),
+				author: this.get('commentAuthor'),
+				body: this.get('commentBody'),
+				idea: model.get('id'),
+				score: Number(String(Math.random()).slice(2,3))
+			})
+
+			this.set('commentAuthor', '');
+			this.set('commentBody', '');
+		}
+	}
+});
