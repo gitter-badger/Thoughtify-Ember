0eadf53d091d745c46de49830797dd2fc42525f2
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-10 23:37:51 -0500

    Adding models. Updating fixture data.

--- app/models/comment.js
+++ app/models/comment.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 var Comment = DS.Model.extend({
 	body: DS.attr('string'),
-	author: DS.attr('string'),
+	author: DS.belongsTo('user', {async: true}),
 	score: DS.attr('number'),
 	idea: DS.belongsTo('idea', {async: true})
 });
@@ -11,35 +11,35 @@ Comment.reopenClass({
 	FIXTURES: [
 		{
 			"id": "54af5ec8f2dbfd53cb0dd87c",
-			"author": "Marietta Rojas",
+			"author": "54b1f153967843d0e59c9723",
 			"body": "Commodo magna est est exercitation exercitation officia sunt consequat aliquip nisi velit. Consequat id deserunt occaecat ea. Duis velit ipsum tempor ea duis aliquip quis velit labore amet dolore. Dolor duis anim qui non. Enim duis eiusmod dolore non exercitation nostrud non non officia esse nulla magna.\r\n",
 			"score": -2,
 			"idea": "54af5ef0d464402a75af9cb5"
 		},
 		{
 			"id": "54af5ec832fb423e8e3a2a87",
-			"author": "Mcdonald Banks",
+			"author": "54b1f153967843d0e59c9723",
 			"body": "Consectetur amet sunt mollit pariatur id aliqua eiusmod quis magna voluptate labore consectetur veniam. Laboris velit duis occaecat pariatur officia proident non proident officia proident. Sint do nulla ut duis laboris ea magna nulla fugiat occaecat irure. Dolor aliquip aliquip culpa reprehenderit tempor pariatur incididunt ex id qui magna ea.\r\n",
 			"score": -4,
 			"idea": "54af5ef0d464402a75af9cb5"
 		},
 		{
 			"id": "54af5ec8d1164b7d7533ff74",
-			"author": "Angelique Mcclure",
+			"author": "54b1f153967843d0e59c9723",
 			"body": "Exercitation laboris occaecat enim incididunt sunt et culpa excepteur. Eiusmod excepteur aute id mollit excepteur non ad sunt sint proident quis ex ullamco consequat. Officia qui magna proident aliquip ex consectetur labore occaecat cillum irure dolore esse deserunt. Dolor in elit enim excepteur amet laboris irure amet. Duis anim anim ex qui cillum reprehenderit id reprehenderit magna.\r\n",
 			"score": -10,
 			"idea": "54af5ef0d464402a75af9cb5"
 		},
 		{
 			"id": "54af5ec8bbb06efce473b63b",
-			"author": "Cline Acevedo",
+			"author": "54b1f153967843d0e59c9723",
 			"body": "In irure culpa aute nostrud occaecat in elit pariatur. Et cillum voluptate excepteur cillum Lorem qui deserunt eiusmod mollit ex consequat proident irure eu. Culpa enim esse laboris mollit exercitation fugiat magna laboris adipisicing ut eu voluptate consectetur. Cupidatat fugiat ad fugiat Lorem ad qui anim est sint veniam elit. Quis occaecat veniam magna reprehenderit non aliquip consectetur velit. Voluptate cillum ad elit minim. Amet non nulla quis in sit quis irure irure.\r\n",
 			"score": 4,
 			"idea": "54af5ef0d464402a75af9cb5"
 		},
 		{
 			"id": "54af5ec88a84ccf47f986322",
-			"author": "Duke Mcfadden",
+			"author": "54b1f153967843d0e59c9723",
 			"body": "Ut officia duis magna quis excepteur elit enim quis quis. Commodo velit et sint do. In qui quis irure dolor.\r\n",
 			"score": -6,
 			"idea": "54af5ef0d464402a75af9cb5"
---
+++ app/models/user.js
@@ -0,0 +1,72 @@
+import DS from 'ember-data';
+
+var User = DS.Model.extend({
+	email: DS.attr('string'),
+	username: DS.attr('string'),
+	avatar: DS.attr('string', {defaultValue: "http://placehold.it/200x200"}),
+	comments: DS.hasMany('comment', {async: true}),
+	ideas: DS.hasMany('idea', {async: true})
+});
+
+User.reopenClass({
+	FIXTURES: [
+		{
+			"id": "54b1f1539727b4db8462aff1",
+			"username": "Suzette Goodwin",
+			"email": "suzettegoodwin@entality.com",
+			"about": "Laboris tempor cillum nulla ea nostrud dolore exercitation mollit amet et dolore. Non dolor irure nisi veniam pariatur consectetur anim aliquip excepteur velit consectetur. Do non incididunt proident dolore reprehenderit amet aliquip cillum labore voluptate consectetur commodo excepteur.\r\n",
+			"comments": [],
+			"ideas": ["54af5ec88a84ccf47f986322", "54af5ef0ebb3bd7ff7973b42", "54af5ef0e24292e5769e9a55", "54af5ef0d464402a75af9cb5", "54af5ef07e56fa83a6ef2c57"]
+		},
+		{
+			"id": "54b1f153967843d0e59c9723",
+			"username": "Medina Barber",
+			"email": "medinabarber@entality.com",
+			"about": "Qui labore voluptate esse sunt adipisicing occaecat. Aliqua aliqua sint ad pariatur dolore eiusmod adipisicing ut. Qui pariatur ut laboris ipsum ut. Reprehenderit nulla aliquip mollit nisi laboris ex laboris eiusmod enim non. Dolore enim aliqua elit non.\r\n",
+			"comments": ["54af5ec8f2dbfd53cb0dd87c", "54af5ec832fb423e8e3a2a87", "54af5ec8d1164b7d7533ff74", "54af5ec8bbb06efce473b63b", "54af5ec88a84ccf47f986322"],
+			"ideas": []
+		},
+		{
+			"id": "54b1f15343281c65714c674e",
+			"username": "Stein Neal",
+			"email": "steinneal@entality.com",
+			"about": "Consectetur ut ex laboris occaecat cupidatat minim. Ad commodo ex id enim aliquip elit sunt duis amet culpa ex. Elit id ad ut et ut excepteur laboris veniam consequat nulla non et dolor enim. Magna cupidatat occaecat officia reprehenderit proident ea duis.\r\n",
+			"comments": [],
+			"ideas": []
+		},
+		{
+			"id": "54b1f1533cf8ebe029ad41f3",
+			"username": "Georgia Flynn",
+			"email": "georgiaflynn@entality.com",
+			"about": "Labore incididunt adipisicing officia tempor non. Ea laboris aute consectetur eu dolor Lorem non elit esse deserunt aute ea do ullamco. Minim ad sint aute pariatur incididunt esse exercitation minim ex ea adipisicing consequat non ea. Mollit incididunt officia irure aliquip. Ut officia in duis laborum. Aliqua sint id cillum deserunt proident eiusmod sint qui Lorem officia nostrud proident irure magna.\r\n",
+			"comments": [],
+			"ideas": []
+		},
+		{
+			"id": "54b1f15349554bdd02ffe9a9",
+			"username": "Maribel Baker",
+			"email": "maribelbaker@entality.com",
+			"about": "Reprehenderit exercitation pariatur incididunt nulla velit velit laborum labore dolor culpa cupidatat tempor consequat. Officia dolore dolore cillum occaecat laboris labore dolor qui commodo. Consequat irure reprehenderit officia dolor nostrud quis.\r\n",
+			"comments": [],
+			"ideas": []
+		},
+		{
+			"id": "54b1f153db0279fb9135c82c",
+			"username": "Foreman Contreras",
+			"email": "foremancontreras@entality.com",
+			"about": "Ut elit enim aute excepteur. Ipsum consequat excepteur ex tempor qui ullamco ea cupidatat ea veniam. Nisi officia dolore excepteur ullamco laboris quis ipsum eiusmod. Et quis magna dolore tempor labore exercitation sunt nulla commodo esse enim id eiusmod. Incididunt aute laboris incididunt adipisicing consectetur aliquip. Ut deserunt laborum ea ex pariatur. Cillum consectetur ad magna qui tempor commodo ut magna minim adipisicing.\r\n",
+			"comments": [],
+			"ideas": []
+		},
+		{
+			"id": "54b1f1531ceb953046c7c15f",
+			"username": "Rosa Watkins",
+			"email": "rosawatkins@entality.com",
+			"about": "Ipsum proident et tempor excepteur nulla id sunt. Aliquip nisi amet non veniam ea. Cillum duis ex id ut. Sit quis in magna pariatur pariatur cillum dolore sint adipisicing proident. Nostrud veniam dolore ad laborum cupidatat incididunt cillum reprehenderit adipisicing. Adipisicing eiusmod tempor laboris elit proident proident elit officia nulla do in.\r\n",
+			"comments": [],
+			"ideas": []
+		}
+	]
+});
+
+export default User;
