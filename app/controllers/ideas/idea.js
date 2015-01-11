d13aa17ca733784c99defbfa32dae6e03efc96ae
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-11 01:00:42 -0500

    No need to pass in the model as a parameter to the action. Adding a check to make sure the user exists before adding his comment.

--- app/controllers/ideas/idea.js
+++ app/controllers/ideas/idea.js
@@ -4,17 +4,29 @@ export default Ember.Controller.extend({
 	commentAuthor: '',
 	commentBody: '',
 	actions: {
-		postComment: function(model) {
-			this.store.push('comment', {
-				id: String(Math.random()).slice(2),
-				author: this.get('commentAuthor'),
-				body: this.get('commentBody'),
-				idea: model.get('id'),
-				score: Number(String(Math.random()).slice(2,3))
-			});
+		postComment: function() {
 
-			this.set('commentAuthor', '');
-			this.set('commentBody', '');
+			var $ = Ember.$;
+			var self = this;
+
+			this.store.find('user').then(function(users){
+				users = users.filterBy('username', self.get('commentAuthor').trim());
+				if (users.length === 0) {
+					throw new Error('No user with username "' + self.get('commentAuthor') + '" found.');
+				}
+
+				// take the first user and 
+				self.store.push('comment', {
+					id: String(Math.random()).slice(2),
+					author: users[0],
+					body: self.get('commentBody'),
+					idea: self.get('model.id'),
+					score: Number(String(Math.random()).slice(2,3))
+				});
+
+			}).catch(function(reason){
+				console.log('an error occured');
+			});
 		}
 	}
 });
