ac81640af060eb8e160c0c7d1f7a4a1961aa2013
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-15 23:42:26 -0500

    Removing addIdea logic from the ideas.index controller.

--- app/controllers/ideas/index.js
+++ app/controllers/ideas/index.js
@@ -1,18 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	ideaTitle: "",
-	ideaBody: "",
 	actions: {
-		addIdea: function() {
-			this.store.push('idea', {
-				id: String(Math.random()).slice(2),
-				title: this.get('ideaTitle'),
-				body: this.get('ideaBody'),
-			});
-			this.set('ideaTitle', '');
-			this.set('ideaBody', '');
-		},
 		destroyIdea: function(idea) {
 			idea.destroyRecord();
 		},
