882efe9d2c68b30a541c6caf9f49b0ebb518711d
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-17 19:57:54 -0500

    Adding action to handle loading of user profile.

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		return this.store.find('user');
+	},
+	actions: {
+		showUserProfile: function(user) {
+			this.transitionTo('users.user', user);
+		}
 	}
 });
