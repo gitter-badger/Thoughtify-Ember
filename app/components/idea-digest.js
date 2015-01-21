7dea5e23ca490771c3c1c5ae528c10aa917043c8
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-01-20 19:36:38 -0500

    Removing the unused close action in the idea-digest component.

--- app/components/idea-digest.js
+++ app/components/idea-digest.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	actions: {
-		close: function(){
-			this.sendAction('close', this.get('idea'));
-		},
 		clickTitle: function(){
 			this.sendAction('clickTitle', this.get('idea'));
 		}
